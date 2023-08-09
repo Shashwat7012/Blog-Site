d1f97f7745e463d8c33f2ced30d35494057fc2ad
Shashwat Pandey <105585782+Shashwat7012@users.noreply.github.com>
2023-08-09 10:33:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const _ = require("lodash");
+const mongoose = require("mongoose");
+const mongodb = require("mongodb");
 
 const homeStartingContent = "Hii, I'm Shashwat Pandey. " +
 "In this  BLOG SITE:- I share my personal blogs";
@@ -16,7 +18,16 @@ app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-let posts = [];
+mongoose.connect('mongodb://localhost:27017/Blogs', { useNewUrlParser: true, useUnifiedTopology: true });
+const blogsSchema = {
+    title: String,
+    content: String
+};
+
+const blogs = mongoose.model("newblog",blogsSchema);
+
+// let posts = [];
+let newBlog;
 
 app.get("/",(req,res)=>{
     res.render("home.ejs",{homePage: homeStartingContent});
@@ -41,47 +52,61 @@ app.get("/compose",(req,res)=>{
     res.render("compose.ejs");
 })
 
-app.get("/posts",(req,res)=>{
-
-    res.render("posts.ejs",{post: posts});
-    console.log(posts);
-})
-
-app.post("/compose",(req,res)=>{
-    // const title = req.body.title;
-    // const post = req.body.post;
-    // console.log(title);
-    // console.log(post);
-    const post = {
-        title: req.body.title,
-        contant: req.body.post
-    };
-    posts.push(post);
-    res.redirect("/posts")
+app.get("/posts", async (req, res) => {
+    try {
+        const foundPosts = await blogs.find({});
+        res.render("posts.ejs", { post: foundPosts }); // Pass the variable as "post"
+    } catch (err) {
+        console.error("Error fetching blog posts:", err);
+        // Handle the error appropriately
+    }
 });
 
-app.get("/posts/:postName",(req,res)=>{
-    const requestedTitle = _.lowerCase(req.params.postName);
+app.post("/compose", (req, res) => {
+    const post = {
+        title: req.body.title,
+        content: req.body.post
+    };
 
-    posts.forEach(function(post){
-        const storesTitle = _.lowerCase(post.title);
-        if(requestedTitle === storesTitle){
-            res.render("post",{
-                title:post.title,
-                contant:post.contant
-            })
-            // console.log("Match Found")
+    const newBlog = new blogs({
+        title: post.title,
+        content: post.content
+    });
+
+    newBlog.save()
+        .then(() => {
+            res.redirect("/posts");
+        })
+        .catch(err => {
+            console.error("Error saving blog post:", err);
+            // Handle the error appropriately
+        });
+});
+
+
+app.get("/posts/:postName", async (req, res) => {
+    try {
+        const requestedTitle = _.lowerCase(req.params.postName);
+
+        const foundPost = await blogs.findOne({ title: requestedTitle });
+
+        if (foundPost) {
+            res.render("post", {
+                title: foundPost.title,
+                content: foundPost.content
+            });
+        } else {
+            // Handle the case when no post is found
+            res.render("post", {
+                title: "Post Not Found",
+                content: "The requested post was not found."
+            });
         }
-        // else{
-        //     console.log("No Match");
-        // }
-    })
-
-})
-
-
-
-
+    } catch (err) {
+        console.error("Error fetching blog post:", err);
+        // Handle the error appropriately
+    }
+});
 
 
 
